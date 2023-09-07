1ce608c37a9ad75b39007f59bd12cdf5b5b1bb8d
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-09-07 13:10:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     logger.info('Bot started')
     bot_token = "6265217004:AAE_yot4SqUFrvBa1IJXgRy-kaeZm9z3Xwk"  # Replace with your actual bot token
-    updater = Updater(bot_token)
+    updater = Updater(bot_token, update_queue=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
